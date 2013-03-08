5447e3db8d2d5ab1ef08b65c10a7ee6c4539c541
Igor Serebryany <igor.serebryany@airbedandbreakfast.com>
2013-03-08 00:39:03 -0800

    use the store in the sinatra app

--- optica.rb
+++ optica.rb
@@ -1,11 +1,42 @@
 require 'sinatra/base'
+require 'json'
+require 'cgi'
 
 class Optica < Sinatra::Base
   get '/' do
-      "no nodes registered yet!"
+    params = CGI::parse(request.query_string)
+
+    # include only those nodes that match passed-in parameters
+    to_return = {}
+    settings.store.nodes.each do |node, properties|
+      included = true
+
+      params.each do |param, values|
+        values.each do |value|
+          if not properties.include? param
+            included = false
+          elsif properties[param].is_a? String
+            included = false unless properties[param] == value
+          elsif properties[param].is_a? Array
+            included = false unless properties[param].include? value
+          end
+        end
+      end
+
+      to_return[node] = properties if included
+    end
+
+    return to_return.to_json
   end
 
   post '/' do
-    "you got it, buddy"
+    begin
+      data = JSON.parse request.body.read
+    rescue JSON::ParserError
+      data = {}
+    end
+
+    settings.store.add(request.ip, data)
+    return 'stored'
   end
 end
