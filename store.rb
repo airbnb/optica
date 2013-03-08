4a21a17dc1e626f017358947cf66a5444b6e36ee
Igor Serebryany <igor.serebryany@airbedandbreakfast.com>
2013-03-08 10:44:56 -0800

    sync thread allows buffering reads and writes

--- store.rb
+++ store.rb
@@ -1,62 +1,118 @@
 require 'zk'
 require 'json'
+require 'logger'
 
 class Store
   attr_reader :nodes
 
   def initialize(opts)
-    unless opts['zk_path']
-      raise ArgumentError, "you need to specify required argument 'zk_path'"
-    else
-      @path = opts['zk_path']
+    @log = Logger.new(STDOUT)
+    @log.progname = self.class.name
+    @log.level = Logger::INFO unless opts['debug']
+
+
+    %w{zk_path aws_access_key aws_secret_key}.each do |req|
+      raise ArgumentError, "missing required argument '#{req}'" unless opts[req]
     end
 
+    @path = opts['zk_path']
+    @aws_key = opts['aws_key']
+    @aws_secret = opts['aws_secret']
+
+    @sync_interval = 30
+    @sync_interval = opts['sync_interval'].to_i if opts.include?('sync_interval')
+
     @nodes = {}
+    @new_nodes = {}
+    @stopping = false
   end
 
   def start()
-    puts "waiting to connect to zookeeper at #{@path}"
+    @log.info "waiting to connect to zookeeper at #{@path}"
     @zk = ZK.new(@path)
-    refresh
+    @zk.ping?
+
+    @log.info "ZK connection established successfully"
+    Thread.new{sync}
   end
 
   def add(node, data)
     @nodes[node] = data
-
-    puts "writing to zk at #{node} with #{data.inspect}"
-
-    node.insert(0, '/')
-    data = data.to_json
-    begin
-      @zk.set(node, data)
-    rescue ZK::Exceptions::NoNode => e
-      @zk.create(node, :data =>data)
-    end
+    @new_nodes[node] = data
   end
 
   def delete(node)
-    puts "deleting node #{node}"
+    @log.info "deleting node #{node}"
     @nodes.delete(node)
 
     @zk.delete(node.insert(0,'/'), :ignore => :no_node)
   end
 
-  def refresh()
-    new_nodes = {}
+  private
+  def sync()
+    @log.debug "starting sync thread"
+
+    while not @stopping
+      begin
+        @log.debug "starting sync"
+
+        do_sync
+        @log.info "sync complete, sleeping for #{@sync_interval}"
+
+        sleep @sync_interval
+
+      rescue Exception => e
+        @log.error "unexpected exception in store sync thread! #{e.inspect}"
+        start
+        break
+      end
+    end
+
+    @log.info "sync thread exited"
+  end
+
+  def do_sync()
+    # ping zookeeper
+    @log.debug "pinging"
+    @zk.ping?
+
+    # write all new nodes to zk
+    @log.debug "writing new nodes"
+    @new_nodes.each do |node, data|
+      child = "/#{node}"
+      data = data.to_json
+      @log.info "writing to zk at #{child} with #{data}"
+
+      begin
+        @zk.set(child, data)
+      rescue ZK::Exceptions::Nochild => e
+        @zk.create(child, :data =>data)
+      end
+
+      @new_nodes.delete(node)
+    end
+
+    # refresh our nodes list
+    @log.debug "reading new nodes"
+    from_server = {}
 
     @zk.children('/').each do |child|
       begin
         data, stat = @zk.get("/#{child}")
-        new_nodes[child] = JSON.parse(data)
+        from_server[child] = JSON.parse(data)
       rescue ZK::Exceptions::NoNode
-        next
+        @log.info "child #{child} disappeared"
       rescue JSON::ParserError
-        puts "removing invalid node #{child}: data failed to parse (#{child_info.inspect})"
+        @log.warn "removing invalid node #{child}: data failed to parse (#{child_info.inspect})"
         delete(child)
       end
     end
 
-    @nodes = new_nodes
+    @nodes = from_server
+
+    # TODO: clean up nodes no longer in aws
+    @log.debug "cleaning up nodes in aws"
   end
+
 
 end
