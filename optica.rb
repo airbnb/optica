239006591b8c2fec31b1db6c6bd80f258dde5f8f
Kai Liu <kai.liu@airbnb.com>
2015-01-12 11:29:02 -0800

    Make optica more robust in the event of rabbitmq failure
    Summary:
    In the Dec 18 2014 Incident where Optica was down because of RabbitMQ. However, failing to publishing updates to RabbitMQ in Optica should not bring the whole Optica down.
    
    This diff makes publishing failure as a warning rather than an error.
    
    Test Plan:
    
    Before:
    
    * shutdown local rabbitmq after server startup
    * post message to local optica, which triggers Events.send().
    * since rabbitmq server is not available, an exception was thrown and halt the whole process.
    
    After:
    
    * shutdown local rabbitmq after server startup
    * post message to local optica
    * the process remail alive, with the error message sent to the client: `stored -- [warning] failed to publish to rabbitmq: Unable to connect rabbitmq server`
    
    Signed-off-by: Kai Liu <kai.liu@airbnb.com>

--- optica.rb
+++ optica.rb
@@ -68,15 +68,28 @@ class Optica < Sinatra::Base
       halt(403) unless ip == header.split(',').first
     end
 
+    # update main store
     begin
       merged_data = settings.store.add(ip, data)
-      settings.events.send(merged_data.merge("event" => data))
     rescue
       halt(500)
     end
 
+    # publish update event
+    error_msg = ""
+    begin
+      settings.events.send(merged_data.merge("event" => data))
+    rescue Exception => e
+      error_msg = e.to_s
+    end
+
     content_type 'text/plain', :charset => 'utf-8'
-    return 'stored'
+
+    # If event publishing failed, we treat it as a warning rather than an error.
+    message = 'stored'
+    message += " -- [warning] failed to publish to rabbitmq: #{error_msg}" if error_msg != ""
+
+    return message
   end
 
   delete '/:id' do |id|
